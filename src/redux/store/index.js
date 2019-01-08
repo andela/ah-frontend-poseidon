078e1b73bde621c91f9edbdcdd9d664a24ebaeea
Luleherll <luleherll@gmail.com>
2019-01-08 12:53:37 +0300

    chore(redux):Setup Redux
    - Install Redux related depedencies
    - Setup file structure for React/Redux
    
    [Delivers #162162956]

---
+++ src/redux/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+
+export default function (initialState = {}) {
+  /** This creates the Redux store */
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
+}
