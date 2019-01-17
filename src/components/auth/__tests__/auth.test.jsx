4b435a48c2a7b96430b29a0995ab82c2be7d7cb5
Kizza Naume <naomekizzan@gmail.com>
2019-01-17 11:08:56 +0300

    feat(login page): implement login page. - write utility function to validate form input fields - write actions, reducers  to handle login functionality - write tests

--- src/components/auth/__tests__/auth.test.jsx
+++ src/components/auth/__tests__/auth.test.jsx
@@ -1,19 +1,19 @@
-import { mount } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
-import React from 'react';
-import SignUpForm from '..';
+import { mount } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
+import React from "react";
+import SignUpForm from "..";
 
-
-describe('SignUpForm component', () => {
-  it('renders without crashing', () => {
+describe("SignUpForm component", () => {
+  it("renders without crashing", () => {
     const wrapper = mount(
       <MemoryRouter>
         <SignUpForm
           onChange={jest.fn}
           onSubmit={jest.fn}
+          isSignUp={true}
           formHasError={() => true}
         />
-      </MemoryRouter>,
+      </MemoryRouter>
     );
     expect(wrapper).toHaveLength(1);
   });
--- src/views/__tests__/authView.test.jsx
+++ src/views/__tests__/authView.test.jsx
@@ -1,71 +1,77 @@
-import React from 'react';
-import configureStore from 'redux-mock-store';
-import { shallow, mount } from 'enzyme';
-import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
-import thunk from 'redux-thunk';
-import AuthViewConnected, { AuthViewTest } from '../authView';
+import React from "react";
+import configureStore from "redux-mock-store";
+import { shallow, mount } from "enzyme";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import thunk from "redux-thunk";
+import AuthViewConnected, { AuthViewTest } from "../authView";
 
 const mockSTore = configureStore([thunk]);
-jest.mock('react-notify-toast');
+jest.mock("react-notify-toast");
 const expectedStore = {
   authReducer: {
     signUpFailure: null,
-    signUpSuccess: null,
-  },
+    signUpSuccess: null
+  }
 };
 
 const store = mockSTore(expectedStore);
 const mockSignUp = {
-  Message: 'Successfully signed up',
-  token: '312regwh4tr4hetrj6y5tu6yutu7y8u',
+  Message: "Successfully signed up",
+  token: "312regwh4tr4hetrj6y5tu6yutu7y8u"
 };
 const mockError = {
   errors: {
-    username: ['Username already exists '],
-  },
+    username: ["Username already exists "]
+  }
 };
 const mockErrors = {
   errors: {
-    username: ['Username already exists '],
-    password: ['password should be at-least 8 characters'],
-  },
+    username: ["Username already exists "],
+    password: ["password should be at-least 8 characters"]
+  }
 };
+
+const errorItems = [
+  { title: "component will recieve props single error", error: mockError },
+  { title: "component will recieve props multiple error", error: mockErrors }
+];
 const props = {
   postDataThunkNoHeader: jest.fn(),
   signUpActionCreatorSuccess: jest.fn(),
   signUpActionCreatorFailure: jest.fn()
 };
 
-describe('authView component', () => {
-  it('it renders ', () => {
+describe("authView component", () => {
+  it("it renders ", () => {
     const wrapper = mount(
       <Provider store={store}>
         <AuthViewConnected props={props} />
-      </Provider>,
+      </Provider>
     );
     const event = {
       preventDefault: jest.fn(),
+      handleSignup: jest.fn(),
       target: {
         elements: {
-          username: { value: 'poseidon' },
-          email: { value: 'poseidon@mail.com' },
-          password: { value: 'poseidon234' },
-        },
-      },
+          username: { value: "poseidon" },
+          email: { value: "poseidon@mail.com" },
+          password: { value: "poseidon234" }
+        }
+      }
     };
-    const wrappedForm = wrapper.find('form');
-    wrappedForm.simulate('submit', event);
-    expect(wrapper.find('AuthView').state('username')).toEqual('poseidon');
+    const wrappedForm = wrapper.find("form");
+    wrappedForm.simulate("submit", event);
+    expect(wrapper.find("AuthView").state("loader")).toEqual({ loading: true });
   });
 
-  it('renders authView without crashing', () => {
+  it("renders authView without crashing", () => {
     const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <AuthViewConnected props={props} />
-          </MemoryRouter>
-        </Provider>,
+      <Provider store={store}>
+        <MemoryRouter>
+          <AuthViewConnected props={props} />
+        </MemoryRouter>
+      </Provider>
     );
     const newProps = { signUpData: mockSignUp };
     AuthViewConnected.prototype.props = newProps;
@@ -73,33 +79,73 @@ describe('authView component', () => {
     expect(wrapper).toHaveLength(1);
   });
 
-  it('component will recieve props user', () => {
-    const wrapUser = shallow(<AuthViewTest {...props } />);
-    wrapUser.setProps({
-      signUpData: mockSignUp,
-      signUpErrors: null,
-    }, () => {
-      expect(wrapUser.state()).toEqual(expect.objectContaining({ loader: { loading: false } }));
-    });
+  it("component will recieve props user", () => {
+    const wrapUser = shallow(<AuthViewTest {...props} />);
+    wrapUser.setProps(
+      {
+        signUpData: mockSignUp,
+        signUpErrors: null
+      },
+      () => {
+        expect(wrapUser.state()).toEqual(
+          expect.objectContaining({ loader: { loading: false } })
+        );
+      }
+    );
   });
 
-  it('component will recieve props single error', () => {
-    const wrapError = shallow(<AuthViewTest {...props} />);
-    wrapError.setProps({
-      signUpData: null,
-      signUpErrors: mockError,
-    }, () => {
-      expect(wrapError.state()).toEqual(expect.objectContaining({ loader: { loading: false } }));
-    });
+  errorItems.forEach(item =>
+    it(item.title, () => {
+      const wrapError = shallow(<AuthViewTest {...props} />);
+      wrapError.setProps(
+        {
+          signUpData: null,
+          signUpErrors: item.error
+        },
+        () => {
+          expect(wrapError.state()).toEqual(
+            expect.objectContaining({ loader: { loading: false } })
+          );
+        }
+      );
+    })
+  );
+});
+
+describe(" Chnages isSignUp to false on click", () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <AuthViewConnected props={props} />
+    </Provider>
+  );
+  const event = {
+    preventDefault: jest.fn(),
+    handleSignup: jest.fn(),
+    target: {
+      elements: {
+        username: { value: "poseidon" },
+        password: { value: "poseidon234" }
+      }
+    }
+  };
+  const wrapped = wrapper.find("#change-login");
+
+  it("isSignup changes to false when handleChangeFormStatus is activated", () => {
+    wrapped.simulate("click", event);
+    expect(wrapper.find("AuthView").state("isSignUp")).toEqual(false);
   });
 
-  it('component will recieve props single error', () => {
-    const wrapError = shallow(<AuthViewTest {...props} />);
-    wrapError.setProps({
-      signUpData: null,
-      signUpErrors: mockErrors,
-    }, () => {
-      expect(wrapError.state()).toEqual(expect.objectContaining({ loader: { loading: false } }));
+  it("isSignup changes to true when handleChangeFormStatus is activated", () => {
+    wrapped.simulate("click", event);
+    expect(wrapper.find("AuthView").state("isSignUp")).toEqual(true);
+  });
+
+  it("handleLogin should be called on login ", () => {
+    wrapped.simulate("click", event);
+    const wrappedForm = wrapper.find("form");
+    wrappedForm.simulate("submit", event);
+    expect(wrapper.find("AuthView").state("loader")).toEqual({
+      loading: true
     });
   });
 });
